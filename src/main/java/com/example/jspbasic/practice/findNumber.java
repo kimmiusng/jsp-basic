1e520e19d067f190233d7f479cc0b688612633e5
kimmisung <mmmmme814@gamil.com>
2023-11-19 20:19:47 +0900

    선택과제 1, 숫자를 찾아주세요

---
+++ src/main/java/com/example/jspbasic/practice/findNumber.java
@@ -0,0 +1,25 @@
+package com.example.jspbasic.practice;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/find-number")
+public class findNumber extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+
+        String input = req.getParameter("input");
+
+        String result = input.replaceAll("[^0-9]","");
+        out.println(result);
+
+
+    }
+}
